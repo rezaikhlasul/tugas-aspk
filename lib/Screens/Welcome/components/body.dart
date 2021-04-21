34fae51ced90be441aade02f9906c856abb41cd3
rezaikhlasul <rezaikhlasul@gmail.com>
2021-04-21 11:26:33 +0700

    login page wip

--- lib/Screens/Welcome/components/body.dart
+++ lib/Screens/Welcome/components/body.dart
@@ -1,4 +1,6 @@
 import 'package:bits/Screens/Welcome/components/background.dart';
+import 'package:bits/components/buttons/auth/button_auth.dart';
+import 'package:bits/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -11,11 +13,38 @@ class Body extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          SvgPicture.asset("assets/icons/chat.svg"),
-          Text(
-            "Selamat datang di BITS",
-            style: TextStyle(
-                fontWeight: FontWeight.bold, fontStyle: FontStyle.italic),
+          Container(
+            margin: EdgeInsets.only(bottom: 50, top: size.height * 0.08),
+            child: SvgPicture.asset(
+              "assets/icons/chat.svg",
+              height: size.height * 0.4,
+            ),
+          ),
+          Container(
+            child: Text(
+              "Selamat datang di BITS",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 20),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 20),
+            child: Column(
+              children: [
+                RoundedAuthButton(
+                  text: "Login",
+                  press: () {},
+                ),
+                RoundedAuthButton(
+                  text: "Sign Up",
+                  color: kPrimaryLightColor,
+                  textColor: Colors.black87,
+                  press: () {},
+                )
+              ],
+            ),
           ),
         ],
       ),
